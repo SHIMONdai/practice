e2292e9306c37baf5b2d2707bab747edde84241b
DaisukeShimobayashi <daisukekarin8@icloud.com>
2022-01-06 01:42:19 +0900

    [add]rand()

--- omikuji.js
+++ omikuji.js
@@ -2,3 +2,8 @@ let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor( Math.random() * 5);
+if (rand == 0) {
+  userresult = "大吉";
+}
